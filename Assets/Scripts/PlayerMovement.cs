93530c54757f07b7ef0151b6e88d90bbfed34d46
Dread-Code <lucasospina97@gmail.com>
2022-10-23 19:36:18 -0500

    Phisics added to mushrooms for bouncing and player for avoiding friction

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -54,7 +54,6 @@ public class PlayerMovement : MonoBehaviour
         myRigidbody.velocity = playerVelocity;
         bool playerHasHorizontalSpeed = Mathf.Abs(myRigidbody.velocity.x) > Mathf.Epsilon;
         myAnimator.SetBool("isRunning", playerHasHorizontalSpeed);
-
     }
 
     private void FlipSprite()
