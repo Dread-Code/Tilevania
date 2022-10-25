f331039c9d71713d146f832fb8c94ed52fb5115a
Dread-Code <lucasospina97@gmail.com>
2022-10-24 22:54:15 -0500

    Game session created

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -115,6 +115,7 @@ public class PlayerMovement : MonoBehaviour
             isAlive = false;
             myAnimator.SetTrigger("Dying");
             myRigidbody.velocity = deathKick;
+            FindObjectOfType<GameSession>().ProcessPlayerDeath();
         }
     }
 }
